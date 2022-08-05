178641c96188df81188d0cd698bf4c206b01d1d0
Shamitbh <shamit.bhatia2@gmail.com>
2022-08-05 00:54:43 -0700

    Added parsing functionality to get news story title and link and return it in dictionary format

--- WebScraper/scrape.py
+++ WebScraper/scrape.py
@@ -4,11 +4,19 @@ from bs4 import BeautifulSoup
 # Want to web scrape all stories (title + link) that have > 100 points
 
 res = requests.get('https://news.ycombinator.com/news')
-#print(res.text)
 
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titlelink')
 votes = soup.select('.score')
 
-print(links[0])
-print(votes[0])
+
+# loop through links and votes and only get title and score
+def create_custom_hackernews(links, votes):
+    hackernews = []
+    for i, link in enumerate(links):
+        title = links[i].getText()
+        href = links[i].get('href', None)
+        hackernews.append({'title': title, 'link': href})
+    return hackernews
+
+print(create_custom_hackernews(links, votes))
