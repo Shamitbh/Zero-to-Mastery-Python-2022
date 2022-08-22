ae8019cd5bd1438ba8af233f14acf8c3974ed711
Shamitbh <shamit.bhatia2@gmail.com>
2022-08-22 11:58:24 -0700

    Created database file to store messages with POST from contact me webpage

--- WebServer/server.py
+++ WebServer/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,20 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong'
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        name = data["name"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{name}, {email}, {subject}, {message}')
